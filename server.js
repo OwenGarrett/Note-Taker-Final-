99872fae6b3b5108b0d3c3b5d8f990a2449967a7
Sheogorath9 <ogarrett189@gmail.com>
2023-01-22 10:59:07 -0500

    set up get routes. only task left is set up post route

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const path = require("path");
-
-
+const notesDb = require("./db/db.json");
+//console.log("this is the notes.db",notesDb)
 const app = express();
 const PORT = 3001; 
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-/* 
-app.get("/contact", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/pages/feedback.html"))
+app.get("/api/notes", (req, res) => {
+  res.json(notesDb)
+});
+
+app.post("/api/notes", (req, res) => {
+  
+});
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-*/
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
